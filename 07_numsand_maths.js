5f5c761977e408815527182d9cccbaea163465fc
Rohit0III <rohitparik172003@gmail.com>
2024-11-22 15:32:50 +0530

    maths library

---
+++ 07_numsand_maths.js
@@ -0,0 +1,19 @@
+// const score=423
+
+// console.log(score);
+
+// const balance =new Number(233)
+// console.log(balance);
+
+// console.log(balance.toFixed(3));
+
+// const otnum= 4234
+// console.log(otnum.toPrecision(5));
+
+
+// const key= 100000
+// console.log(key.toLocaleString('en-IN'))
+// ---------------------------------------------------------------------------------------------------
+
+console.log(Math);
+
