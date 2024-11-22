3d4ab44eec9fe63ae99118895739034c2520290c
Rohit0III <rohitparik172003@gmail.com>
2024-11-22 10:35:19 +0530

    updated data conversion

---
+++ 03_Datatypeconversion.js
@@ -0,0 +1,28 @@
+let  score = null;
+
+// console.log(typeof(score));
+// console.log(typeof(sc));
+
+
+// let valueinnum= Number(score)
+// console.log(typeof valueinnum)
+// console.log(valueinnum);
+
+
+// "33" =>33
+// "33dxf"=>NaN
+// // true =>1 or false=>0
+
+// // let islogggedin =1
+// let islogggedin ='rohan'
+// let booleaninlogged = Boolean(islogggedin)
+// console.log(booleaninlogged);
+// //  ""=>false
+// "rohan"=>true
+
+
+let snum= 33
+let stnum=String(snum)
+console.log(stnum);
+console.log(typeof stnum);
+
