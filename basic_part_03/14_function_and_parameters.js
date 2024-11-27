ce2e2dd0a2120b3f3ad464e7fb3979119c1643eb
Rohit0III <rohitparik172003@gmail.com>
2024-11-27 14:21:45 +0530

    ....

--- basic_part_03/14_function_and_parameters.js
+++ basic_part_03/14_function_and_parameters.js
@@ -20,6 +20,22 @@ function add(n1,n2) {
     return result; 
 }
 
-console.log(result)
+// console.log(result)
+
+function loginmesaage(username ="sam") {
+    if ( !username) {
+       return console.log("write a user name")   
+    }
+    return`${username} just logged`
+}
 
 
+// const re =loginmesaage()
+// console.log(re);
+
+
+
+
+function price(){
+    
+}
