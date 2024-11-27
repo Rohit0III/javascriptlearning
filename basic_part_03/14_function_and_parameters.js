18994f670fb038eb5b8ef2afc3b38ca00ff93ecc
Rohit0III <rohitparik172003@gmail.com>
2024-11-27 13:58:06 +0530

    ....

---
+++ basic_part_03/14_function_and_parameters.js
@@ -0,0 +1,25 @@
+
+function greet() {
+    console.log("h");
+    console.log("i");
+}
+
+
+// greet (refrence)
+
+// greet() //(execution)
+
+// function add(n1,n2) {
+//     console.log(n1+n2);   
+// }
+
+
+const result = add(5,4)
+function add(n1,n2) {
+    let result =n1+n2
+    return result; 
+}
+
+console.log(result)
+
+
